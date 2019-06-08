2b7c1ba929f474a2a1f755c43152fe93f5254be0
Breno Caldeira <breno.caldeira@gmail.com>
2019-06-08 11:27:03 -0300

    Assign mock_movie with main and creates empty Movie Details screen.

---
+++ lib/screens/movie_details_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie/data/movie.dart';
+
+class MovieDetailsPage extends StatelessWidget {
+  MovieDetailsPage({@required this.movie});
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('AAAA'),
+    );
+  }
+}
