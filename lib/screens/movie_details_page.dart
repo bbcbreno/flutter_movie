abf2cf61f21002d760ad4b4bdf20cd69d6f83163
Breno Caldeira <breno.caldeira@gmail.com>
2019-06-08 12:14:43 -0300

    Add MovieDetailsPage's structure.

--- lib/screens/movie_details_page.dart
+++ lib/screens/movie_details_page.dart
@@ -5,10 +5,45 @@ class MovieDetailsPage extends StatelessWidget {
   MovieDetailsPage({@required this.movie});
   final Movie movie;
 
+  Widget _movieDetailHeader() {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          movie.bannerUrl,
+        ),
+      ],
+    );
+  }
+
+  Widget _storyline() {
+    return Text(movie.storyline);
+  }
+
+  Widget _photoScroller() {
+    return Placeholder();
+  }
+
+  Widget _actorScroller() {
+    return Placeholder();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('AAAA'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _movieDetailHeader(),
+            SizedBox(height: 20.0),
+            _storyline(),
+            SizedBox(height: 20.0),
+            _photoScroller(),
+            SizedBox(height: 20.0),
+            _actorScroller(),
+            SizedBox(height: 50.0)
+          ],
+        ),
+      ),
     );
   }
 }
