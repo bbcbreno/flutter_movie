a58de03eb69f9c3e65055e2d3874e91f9c4df091
Breno Caldeira <breno.caldeira@gmail.com>
2019-06-10 15:41:44 -0300

    Add storyline widget style.

--- lib/screens/movie_details_page.dart
+++ lib/screens/movie_details_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie/data/movie.dart';
 import 'package:flutter_movie/ui/movie_detail_header.dart';
+import 'package:flutter_movie/ui/storyline.dart';
 
 class MovieDetailsPage extends StatelessWidget {
   MovieDetailsPage({@required this.movie});
   final Movie movie;
-
-  Widget _storyline() {
-    return Text(movie.storyline);
-  }
 
   Widget _photoScroller() {
     return Placeholder();
@@ -29,7 +26,7 @@ class MovieDetailsPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(20.0),
-              child: _storyline(),
+              child: Storyline(movie: movie),
             ),
             _photoScroller(),
             SizedBox(height: 20.0),
